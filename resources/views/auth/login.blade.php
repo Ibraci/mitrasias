3497f1d7c4f4134504c09cdb297083f8d37e1967
Ibrahim CISSE <ibraci1960@gmail.com>
2018-01-25 11:22:32 +0000

    Customization of login page

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,69 +1,45 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
+    <span class="card-title">Sign In</span>
     <div class="row">
-        <div class="col-md-8 col-md-offset-2">
-            <div class="panel panel-default">
-                <div class="panel-heading">Login</div>
+        <form class="col s12" method="POST" action="{{ route('login') }}">
+            {{ csrf_field() }}
+            <div class="input-field col s12 {{ $errors->has('email') ? ' has-error' : '' }}">
+                <input id="email" name="email" type="email" value="{{ old('email') }}" required autofocus>
+                <label for="email">Email</label>
 
-                <div class="panel-body">
-                    <form class="form-horizontal" method="POST" action="{{ route('login') }}">
-                        {{ csrf_field() }}
-
-                        <div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">
-                            <label for="email" class="col-md-4 control-label">E-Mail Address</label>
-
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control" name="email" value="{{ old('email') }}" required autofocus>
-
-                                @if ($errors->has('email'))
-                                    <span class="help-block">
-                                        <strong>{{ $errors->first('email') }}</strong>
-                                    </span>
-                                @endif
-                            </div>
-                        </div>
-
-                        <div class="form-group{{ $errors->has('password') ? ' has-error' : '' }}">
-                            <label for="password" class="col-md-4 control-label">Password</label>
-
-                            <div class="col-md-6">
-                                <input id="password" type="password" class="form-control" name="password" required>
-
-                                @if ($errors->has('password'))
-                                    <span class="help-block">
-                                        <strong>{{ $errors->first('password') }}</strong>
-                                    </span>
-                                @endif
-                            </div>
-                        </div>
-
-                        <div class="form-group">
-                            <div class="col-md-6 col-md-offset-4">
-                                <div class="checkbox">
-                                    <label>
-                                        <input type="checkbox" name="remember" {{ old('remember') ? 'checked' : '' }}> Remember Me
-                                    </label>
-                                </div>
-                            </div>
-                        </div>
-
-                        <div class="form-group">
-                            <div class="col-md-8 col-md-offset-4">
-                                <button type="submit" class="btn btn-primary">
-                                    Login
-                                </button>
-
-                                <a class="btn btn-link" href="{{ route('password.request') }}">
-                                    Forgot Your Password?
-                                </a>
-                            </div>
-                        </div>
-                    </form>
-                </div>
+                @if ($errors->has('email'))
+                    <span class="help-block">
+                        <strong>{{ $errors->first('email') }}</strong>
+                    </span>
+                @endif
             </div>
-        </div>
+            <div class="input-field col s12 {{ $errors->has('password') ? ' has-error' : '' }}">
+                <input id="password" name="password" type="password" class="validate">
+                <label for="password">Password</label>
+
+                @if ($errors->has('password'))
+                    <span class="help-block">
+                        <strong>{{ $errors->first('password') }}</strong>
+                    </span>
+                @endif
+            </div>
+
+            <div class="col s12">
+                <input id="checkbox" name="checkbox" type="checkbox" class="validate" {{ old('remember') ? 'checked' : '' }}>
+                <label for="checkbox">Remember Me</label>
+            </div>
+
+            <div class="col s12 right-align m-t-sm">
+                <button class="waves-effect waves-light btn teal" name="sign_in" type="submit">sign in</button>
+            </div>
+
+            <div class="col s12 left-align m-t-sm">
+                <a class="" href="{{ route('password.request') }}">
+                    Forgot Your Password?
+                </a>
+            </div>
+        </form>
     </div>
-</div>
 @endsection
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,80 +1,59 @@
 <!DOCTYPE html>
-<html lang="{{ app()->getLocale() }}">
-<head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+<html lang="en">
+    <head>
+        <!-- CSRF Token -->
+        <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <!-- CSRF Token -->
-    <meta name="csrf-token" content="{{ csrf_token() }}">
+        <title>{{ config('app.name', 'MitrasIAS') }}</title>
 
-    <title>{{ config('app.name', 'Laravel') }}</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no"/>
+        <meta charset="UTF-8">
+        <meta name="description" content="Responsive Admin Dashboard Template" />
+        <meta name="keywords" content="admin,dashboard" />
+        <meta name="author" content="Ibrahim CISSE (Ibraci)" />
+        <!-- Styles -->
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.100.2/css/materialize.min.css">
+        <link rel="stylesheet" href="http://fonts.googleapis.com/icon?family=Material+Icons">
+        <link rel="stylesheet" href="{{asset('assets/plugins/metrojs/MetroJs.min.css')}}">
+        <link rel="stylesheet" href="{{asset('assets/plugins/weather-icons-master/css/weather-icons.min.css')}}">
+        <!-- Theme Styles -->
+        <link rel="stylesheet" type="text/css" href="{{asset('assets/css/alpha.min.css')}}"/>
+        <link rel="stylesheet" type="text/css" href="{{asset('assets/css/custom.css')}}"/>
+        <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.10.16/css/jquery.dataTables.min.css">
+        <!-- Pick a theme, load the plugin & initialize plugin -->
+        <script src="{{asset('assets/plugins/jquery/jquery-2.2.0.min.js')}}"></script>
+        <script type="text/javascript" src="https://cdn.datatables.net/1.10.16/js/jquery.dataTables.min.js"></script>
+    </head>
+    <body>
+        <div class="signin-page">
+            <div class="mn-content valign-wrapper">
+                <main class="mn-inner container">
+                    <div class="valign">
+                        <div class="row">
+                            <div class="col s12 m6 l4 offset-l4 offset-m3">
+                                <div class="card white darken-1">
+                                    <div class="card-content ">
 
-    <!-- Styles -->
-    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
-</head>
-<body>
-    <div id="app">
-        <nav class="navbar navbar-default navbar-static-top">
-            <div class="container">
-                <div class="navbar-header">
-
-                    <!-- Collapsed Hamburger -->
-                    <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#app-navbar-collapse" aria-expanded="false">
-                        <span class="sr-only">Toggle Navigation</span>
-                        <span class="icon-bar"></span>
-                        <span class="icon-bar"></span>
-                        <span class="icon-bar"></span>
-                    </button>
-
-                    <!-- Branding Image -->
-                    <a class="navbar-brand" href="{{ url('/') }}">
-                        {{ config('app.name', 'Laravel') }}
-                    </a>
-                </div>
-
-                <div class="collapse navbar-collapse" id="app-navbar-collapse">
-                    <!-- Left Side Of Navbar -->
-                    <ul class="nav navbar-nav">
-                        &nbsp;
-                    </ul>
-
-                    <!-- Right Side Of Navbar -->
-                    <ul class="nav navbar-nav navbar-right">
-                        <!-- Authentication Links -->
-                        @guest
-                            <li><a href="{{ route('login') }}">Login</a></li>
-                            <li><a href="{{ route('register') }}">Register</a></li>
-                        @else
-                            <li class="dropdown">
-                                <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false" aria-haspopup="true">
-                                    {{ Auth::user()->name }} <span class="caret"></span>
-                                </a>
-
-                                <ul class="dropdown-menu">
-                                    <li>
-                                        <a href="{{ route('logout') }}"
-                                            onclick="event.preventDefault();
-                                                     document.getElementById('logout-form').submit();">
-                                            Logout
-                                        </a>
-
-                                        <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
-                                            {{ csrf_field() }}
-                                        </form>
-                                    </li>
-                                </ul>
-                            </li>
-                        @endguest
-                    </ul>
-                </div>
+                                        @yield('content')
+                                        
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </main>
             </div>
-        </nav>
-
-        @yield('content')
-    </div>
-
+        </div>
+        <!-- Javascripts -->
+        <script src="{{asset('assets/plugins/jquery/jquery-2.2.0.min.js')}}"></script>
+        <script src="{{asset('assets/plugins/materialize/js/materialize.min.js')}}"></script>
+        <script src="{{asset('assets/plugins/material-preloader/js/materialPreloader.min.js')}}"></script>
+        <script src="{{asset('assets/plugins/jquery-blockui/jquery.blockui.js')}}"></script>
+        <script src="{{asset('assets/js/alpha.min.js')}}"></script>
+    </body>
+</html>
+{{--
     <!-- Scripts -->
     <script src="{{ asset('js/app.js') }}"></script>
 </body>
-</html>
+</html> --}}
